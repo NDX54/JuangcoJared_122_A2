9508280fe2f4a2e9c33e5c3ebc5ea7e68977aa74
Jared Juangco <jjuangco01@gmail.com>
2022-10-25 19:50:34 +0800

    Implemented all classes TODO - Implement calculation functions like area and perimeter

--- src/A2_S4_2022.java
+++ src/A2_S4_2022.java
@@ -12,11 +12,13 @@ interface ForTwoD {
 }
 
 interface ForThreeD {
+
     double volume();
     void resize(double percentage);
 }
 
 interface Shape {
+    double pi = Math.PI;
     double area();
 }
 
@@ -107,15 +109,153 @@ abstract class TwoD implements ForTwoD, Shape {
         this.c = c;
         this.d = d;
     }
+
+    @Override
+    public void recolor(ShapeColor sc) {
+
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+
 }
 
 abstract class ThreeD implements  ForThreeD, Shape {
     protected ShapeColor sc;
     protected double a;
 
+
+    public ThreeD() {
+
+    }
+
+    public ThreeD(ShapeColor sc, double a) {
+        this.sc = sc;
+        this.a = a;
+    }
+
+    public ThreeD(ThreeD td) {
+        this(td.sc, td.a);
+    }
+
+    double getA() {
+        return a;
+    }
+
+    public void set(ShapeColor sc, double a) {
+        this.sc = sc;
+        this.a = a;
+    }
+
+    public void resize(double percentage) {
+
+
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
+
+class Circle extends TwoD {
+
+    public Circle() {
+
+    }
+
+    public Circle(ShapeColor sc, int radius) {
+        this.sc = sc;
+        this.a = radius;
+    }
+
+    public Circle(Circle c) {
+        this(c.sc, c.a);
+    }
+
+    int getRadius() {
+        return getA();
+    }
+
+    @Override
+    public double perimeter() {
+        return 0;
+    }
+
+    @Override
+    public double area() {
+        return pi;
+    }
+
+    public void set(ShapeColor sc, int radius) {
+        this.sc = sc;
+        this.a = radius;
+    }
+
+    @Override
+    public String toString() {
+        return "Circle (2D (" + getShapeColor() + ", " + getRadius() + "))";
+    }
+}
+
+class Rectangle extends TwoD {
+
+    public Rectangle() {
+
+    }
+
+    public Rectangle(ShapeColor sc, int length, int width) {
+        this.sc = sc;
+        this.a = length;
+        this.b = width;
+    }
+
+    public Rectangle(Rectangle r) {
+        this(r.sc, r.a, r.b);
+    }
+
+    @Override
+    public double perimeter() {
+        return 0;
+    }
+
+    @Override
+    public void recolor(ShapeColor sc) {
+
+    }
+
+    @Override
+    public double area() {
+        return 0;
+    }
 }
 
 
 public class A2_S4_2022 {
+    private static int getInt() {
+        return 0;
+    }
 
+    private static double getDouble() {
+        return 0.0;
+    }
+
+    private static ShapeColor getColor() {
+        return ShapeColor.Green;
+    }
+
+    private static boolean isTriangle(int a, int b, int c) {
+        if (a != 0 && b != 0 && c != 0) {
+            return true;
+        } else  {
+            return false;
+        }
+    }
+    public static void main(String[] args) {
+        Circle testCircle = new Circle(ShapeColor.Blue, 12);
+
+        System.out.println(testCircle);
+    }
 }
